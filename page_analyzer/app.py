eee7c774903bbeb8282b3d08a7590749a00587dc
painkiller2001 <fsvfsvfsva@yandex.by>
2026-06-30 22:22:19 +0300

    Layout, index adj.

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
 
@@ -8,6 +8,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
-@app.route("/")
-def first_hello_func():
-    return f'Hello, man!'
+# @app.route("/")
+# def first_hello_func():
+#     return f'Hello, man!'
+
+
+@app.get("/")
+def get_main_content():
+    return render_template(
+        'index.html'
+        )
